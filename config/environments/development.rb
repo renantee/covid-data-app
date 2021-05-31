fe855dfb8abdb7e7b52ac7156f2d698cfdfe43a7
Renante D. Entera <renante_entera@ligph.com>
2021-05-31 14:01:46 +0800

    Configure devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.raise = true # raise an error if n+1 query occurs
   end
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
