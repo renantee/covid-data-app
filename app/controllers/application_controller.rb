6ba8a694de2444a084b50671ecf391d3f0db40bc
Renante D. Entera <renante_entera@ligph.com>
2021-06-01 17:19:10 +0800

    Update user registration to include “country”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation, :is_admin)
+      user_params.permit(:email, :country_id, :password, :password_confirmation, :is_admin)
     end
   end
 end
