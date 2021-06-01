30efe0b27dfbc99de59881dbe0a5624b19bb0dce
Renante D. Entera <renante_entera@ligph.com>
2021-06-01 15:41:48 +0800

    Generate seed file and data for `countries`
    Run the following:
    > ruby db/fixtures/seed_countries_generator.rb
    > rails db:seed_fu FILTER=countries

---
+++ db/fixtures/seed_countries_generator.rb
@@ -0,0 +1,20 @@
+require "csv"
+require "seed-fu"
+
+# change these to match the seeded model
+@csv_file = "db/fixtures/seed_countries.csv"
+@output_file = "db/fixtures/seed_countries.rb"
+@class_name = "Country"
+
+@seeds = CSV.read(@csv_file, headers: true)
+
+SeedFu::Writer.write(@output_file.to_s, class_name:  @class_name.to_s,
+                                        constraints: [:id]) do |writer|
+  @seeds.each do |seed|
+    hash = {}
+    @seeds.headers.reject { |h| h.nil? }.each do |header|
+      hash[header.to_sym] = seed[header]
+    end
+    writer.add(hash)
+  end
+end
