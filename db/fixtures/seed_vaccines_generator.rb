9c198b4b71312591c9da8ab4782e3e7f0695fff0
Renante D. Entera <renante_entera@ligph.com>
2021-06-01 16:27:20 +0800

    Generate seed file and data for `vaccines`
    Run the following:
    > ruby db/fixtures/seed_vaccines_generator.rb
    > rails db:seed_fu FILTER=vaccines

---
+++ db/fixtures/seed_vaccines_generator.rb
@@ -0,0 +1,24 @@
+require "csv"
+require "seed-fu"
+
+# change these to match the seeded model
+@csv_file = "db/fixtures/seed_vaccines.csv"
+@output_file = "db/fixtures/seed_vaccines.rb"
+@class_name = "Vaccine"
+
+@seeds = CSV.read(@csv_file, headers: true)
+
+SeedFu::Writer.write(@output_file.to_s, class_name:  @class_name.to_s,
+                                        constraints: [:id]) do |writer|
+  @seeds.each do |seed|
+    hash = {}
+    @seeds.headers.reject { |h| h.nil? }.each do |header|
+      hash[header.to_sym] = if header == "data_source"
+                              seed[header] == "REPORTING" ? 0 : 1
+                            else
+                              seed[header]
+                            end
+    end
+    writer.add(hash)
+  end
+end
