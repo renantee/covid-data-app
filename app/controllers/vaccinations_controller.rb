9b1c27e883dcc67b8211cf5d85be7dabbc585b92
Renante D. Entera <renante_entera@ligph.com>
2021-06-16 12:21:14 +0800

    DRYing vaccinations controller

--- app/controllers/vaccinations_controller.rb
+++ app/controllers/vaccinations_controller.rb
@@ -2,9 +2,7 @@ class VaccinationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @vaccinations = Vaccination.filter(params.slice(:country, :data_source, :vaccine,
-                                                    :date_updated_start_date, :date_updated_end_date,
-                                                    :first_vaccine_date_start, :first_vaccine_date_end))
+    @vaccinations = Vaccination.filter(filtering_params)
                                .includes(:vaccines, :country)
 
     @countries = Country.all
@@ -28,14 +26,13 @@ class VaccinationsController < ApplicationController
   private
 
   def file_upload(csv_file)
-    # require 'fileutils'
     tmp = csv_file.tempfile
     @file = File.join("public", csv_file.original_filename)
     FileUtils.cp tmp.path, @file
   end
 
   # A list of the param names that can be used for filtering the vaccinations list
-  def filtering_params(params)
+  def filtering_params
     params.slice(:country, :data_source, :vaccine,
                  :date_updated_start_date, :date_updated_end_date,
                  :first_vaccine_date_start, :first_vaccine_date_end)
