f7c46e17c1f733d37ed30dc988d093f6d0ef9c18
Renante D. Entera <renante_entera@ligph.com>
2021-06-18 11:14:27 +0800

    RSpec Models: User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :rememberable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :validatable
+  validates :country_id, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @country = create(:country)
+    @user = attributes_for(:user)
+    described_class.new(@user)
+  end
+
+  it "is database authenticable" do
+    expect(subject.valid_password?(subject.password)).to be_truthy
+  end
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a country" do
+    subject.country_id = nil
+    expect(subject).to_not be_valid
+  end
 end
