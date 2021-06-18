2b8b197e6dffbf9b223b4ed4a1e5b4e445f6cb78
Renante D. Entera <renante_entera@ligph.com>
2021-06-18 11:02:14 +0800

    RSpec Models: Country

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :vaccinations, dependent: :destroy
+
+  validates :country, :iso2, :iso3, presence: true
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @country = attributes_for(:country)
+    described_class.new(@country)
+  end
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a country" do
+    subject.country = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a iso2" do
+    subject.iso2 = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a iso3" do
+    subject.iso3 = nil
+    expect(subject).to_not be_valid
+  end
 end
