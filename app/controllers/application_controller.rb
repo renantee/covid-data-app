31aed8e14e2811e61c4664be38fcb3a28f5d0afe
Renante D. Entera <renante_entera@ligph.com>
2021-06-21 12:56:48 +0800

    Update registration page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_country_paramaters, if: :devise_controller?
 
   protected
 
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:email, :country_id, :password, :password_confirmation, :is_admin)
     end
+  end
+
+  def configure_country_paramaters
+    @countries = Country.all
+    @default_user_country = Country.find_by(iso2: "PH")
   end
 end
