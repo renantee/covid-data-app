9afacadd352a116ad9c1a77b243acfbf216beff7
Renante D. Entera <renante_entera@ligph.com>
2021-06-21 11:12:34 +0800

    RSpec Models: Vaccination

--- spec/models/vaccination_spec.rb
+++ spec/models/vaccination_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Vaccination, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @country = create(:country)
+    @vaccination = attributes_for(:vaccination, country_id: @country.id)
+    described_class.new(@vaccination)
+  end
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without country id" do
+    subject.country_id = nil
+    expect(subject).to_not be_valid
+  end
 end
