fa4a6c7313f801cfe137bc412841fe5f1b2949e7
Renante D. Entera <renante_entera@ligph.com>
2021-06-23 14:59:47 +0800

    Refactor dashboard redirection

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    redirect_to vaccinations_path if redirect_referer.include? request.referer
+  end
+
+  private
+
+  def redirect_referer
+    ["http://localhost:3000/users/sign_up", "http://localhost:3000/users/sign_in"]
+  end
 end
