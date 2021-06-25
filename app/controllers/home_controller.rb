18a9bed64ca7a1af3eff2e74a728405f3838619b
Renante D. Entera <renante_entera@ligph.com>
2021-06-25 11:49:55 +0800

    Add heroku referer for dashboard redirection

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,13 @@ class HomeController < ApplicationController
   private
 
   def redirect_referer
-    ["http://localhost:3000/users/sign_up", "http://localhost:3000/users/sign_in"]
+    [
+      "http://localhost:3000/users/sign_up",
+      "http://localhost:3000/users/sign_in",
+      "https://nante-covid-data.herokuapp.com/users/sign_up",
+      "https://nante-covid-data.herokuapp.com/users/sign_in",
+      "http://nante-covid-data.herokuapp.com/users/sign_up",
+      "http://nante-covid-data.herokuapp.com/users/sign_in"
+    ]
   end
 end
